d186f7a315a54d61a6b592c159ae32c332acde42
Athina Bekakou <athinabek@grnet.gr>
2015-09-27 00:30:30 +0300

    Move footer, header, top-bar from mocks to proper place

--- app/routes/builds.js
+++ app/routes/builds.js
@@ -9,6 +9,15 @@ export default Ember.Route.extend({
 
   model: function(params){
     return this.store.query('build', params);
+  },
+
+  renderTemplate: function(){
+    this._super();
+    this.render('builds-tools', {
+      into: 'application',
+      outlet: 'top-bar-right',
+      controller: 'builds',
+    });
   }
 
 });
