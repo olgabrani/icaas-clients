06b1a0c6d6d6adc1c6f0e6e65e8abbc1d0eeb7dd
Olga Brani <olgabrani@grnet.gr>
2015-09-30 12:24:43 +0300

    Remove input error message when user types

--- app/components/error-msg.js
+++ app/components/error-msg.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  keyUp: function(){
+    var self = this;
+    let val = this.$().find('input').val();
+    if (is.not.empty(val.trim()) ){
+      self.set('err', null);
+    }
+  }
 });
