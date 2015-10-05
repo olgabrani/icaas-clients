72bade7ac546c4d94cfe798648ffb33a1967cedd
Athina Bekakou <athinabek@grnet.gr>
2015-10-05 16:34:00 +0300

    Modify the search-box
    * Change the style of search box.
    * Set the clear button visible only when the input field is not empty.

--- app/components/search-box.js
+++ app/components/search-box.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   placeholder: 'Filter builds list',
   classNames: ['search-box'],
+  hideClearBtn: function() {
+    var hideClearBtn;
+    hideClearBtn = this.get('value').length ? false : true;
+
+    return hideClearBtn;
+
+  }.property('value'),
+
   actions: {
     'clearSearch': function(){
       this.set('value', '');
