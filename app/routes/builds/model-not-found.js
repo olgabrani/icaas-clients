1b967f03fcf088f7fec9754764e35d6ca5e87304
Olga Brani <olgabrani@grnet.gr>
2015-10-06 16:11:45 +0300

    Handle case when build_id is not an integer

--- app/routes/builds/model-not-found.js
+++ app/routes/builds/model-not-found.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  afterModel(resolvedModel, transition) {
+    // if the build_id is not an intiger redirect to 404 page
+    if (isNaN(resolvedModel.fake)) {
+      var a = transition.intent.url;
+      var b = a.replace(/^[/]/, '');;
+      this.transitionTo('not-found', b);
+    }
+  }
 });
