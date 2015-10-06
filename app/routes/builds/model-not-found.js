33a2de69a7e2b811517fddaeb9af761d6e38a359
Olga Brani <olgabrani@grnet.gr>
2015-10-06 16:43:08 +0300

    Fix typo

--- app/routes/builds/model-not-found.js
+++ app/routes/builds/model-not-found.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   afterModel(resolvedModel, transition) {
-    // if the build_id is not an intiger redirect to 404 page
+    // if the build_id is not an integer redirect to 404 page
     if (isNaN(resolvedModel.fake)) {
       var a = transition.intent.url;
       var b = a.replace(/^[/]/, '');;
