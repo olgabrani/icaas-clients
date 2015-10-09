2147f15aa9847db41955bc580b4d8a5e32dc2830
Athina Bekakou <athinabek@grnet.gr>
2015-10-09 13:17:42 +0300

    Add style rules for cancelled status

--- app/components/build-status.js
+++ app/components/build-status.js
@@ -1,8 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	isCreating: Ember.computed.equal('status', 'CREATING'),
-	isCompleted: Ember.computed.equal('status', 'COMPLETED'),
-	isError: Ember.computed.equal('status', 'ERROR'),
-	classNameBindings: ['isCreating:creating', 'isCompleted:success', 'isError:error'],
+
+    isCreating: Ember.computed.equal('status', 'CREATING'),
+    isCompleted: Ember.computed.equal('status', 'COMPLETED'),
+
+    classNameBindings: ['statusToLowerCase'],
+
+    statusToLowerCase: function() {
+        return this.get('status').toLowerCase();
+    }.property('status')
+
 });
