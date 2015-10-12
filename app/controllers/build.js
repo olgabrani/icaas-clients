d32b024a1b3f0bb92260e5233338deb1f1962b01
Olga Brani <olgabrani@grnet.gr>
2015-10-12 23:16:21 +0300

    Handle log file view
    * Do not open log file in new tab
    * Do not show log file if it is not yet created

--- app/controllers/build.js
+++ app/controllers/build.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import {raw as ajax} from 'ic-ajax';
 
 export default Ember.Controller.extend({
   isCompleted: Ember.computed.equal('model.status', 'COMPLETED'),
@@ -15,12 +16,45 @@ export default Ember.Controller.extend({
     return '*';
   }.property('settings.storag_view_url', 'model.log'),
 
+  log_loader: false,
+  log_contents: null,
+  log_btn_txt: function(){
+    return this.get('log_contents')? 'Hide':'Show';
+  }.property('log_contents'),
+
+  watchModelChange: function(){
+    this.set('log_contents', null);
+  }.observes('model.id'),
+
   actions: {
     cancel() {
       var build = this.get('model');
       build.set('action', 'cancel');
       build.save();
+    },
 
+    toggle_log(){
+      if (this.get('log_contents')) {
+        this.set('log_contents', null);
+        return;
+      }
+      this.send('load_log');
+    },
+
+    load_log() {
+      var self = this;
+      this.set('log_loader', true);
+      return ajax({
+        url: self.get('log_url'),
+        method: 'GET',
+        type: 'json',
+      }).then(function(data) {
+        self.set('log_contents', data.response);
+        self.set('log_loader', false);
+      }, function(err){
+        self.set('log_contents', 'No log available');
+        self.set('log_loader', false);
+      });
     }
   }
 });
