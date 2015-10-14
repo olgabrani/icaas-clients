c1f1036ab328061f8f14cb25e96b3d9e193beb8b
Athina Bekakou <athinabek@grnet.gr>
2015-10-14 14:30:31 +0300

    Keep header style in local storage

--- app/components/demo-header.js
+++ app/components/demo-header.js
@@ -1,23 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	tagName: 'h1',
-	styles: ['font-demo-1', 'font-demo-2', 'font-demo-3', 'font-demo-4'],
-	classNames: ['font-demo-1'],
-	attributeBindings: ['data-present-style', 'title'],
-	'data-present-style': function(){
-    return this.get('styles')[0];
-  }.property(),
-	title: 'Click me to change style!',
+  tagName: 'h1',
+  styles: ['font-demo-1', 'font-demo-2', 'font-demo-3', 'font-demo-4'],
+  classNames: ['font-demo-1'],
+  attributeBindings: ['data-present-style', 'title'],
+  'data-present-style': '',
+  title: 'Click to change me!',
   click: function() {
     var styles = this.get('styles');
     var stylesLength = styles.length;
     var currentStyle = this.get('data-present-style');
     var index = styles.indexOf(currentStyle);
     if(index < stylesLength - 1) {
-      this.set('data-present-style', styles[index + 1]);
+      localStorage.headerStyle = styles[index + 1];
+      this.set('data-present-style', localStorage.headerStyle);
     } else {
-      this.set('data-present-style', styles[0]);
+      localStorage.headerStyle = styles[0];
+      this.set('data-present-style', localStorage.headerStyle);
     }
-  }
+  },
+  detectPreferences: function() {
+    var storedStyle = localStorage.headerStyle;
+    if(localStorage.headerStyle) {
+      this.set('data-present-style', localStorage.headerStyle);
+    }
+    else {
+      localStorage.headerStyle = this.get('styles')[0];
+      this.set('data-present-style', localStorage.headerStyle);
+    }
+  }.on('init')
 });
