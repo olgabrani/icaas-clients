e46db5b8af4986b65d7311c23e2e670a3f17f124
Athina Bekakou <athinabek@grnet.gr>
2015-11-22 21:35:15 +0200

    Add example for production configuration

--- config/environment.js
+++ config/environment.js
@@ -67,7 +67,19 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.appSettings = {
+      'branding': {
+        STORAGE_LOGO_URL: 'https://example.com/static/branding/images/storage_logo.png'
+      },
+      auth_url: 'https://accounts.example.com/identity/v2.0',
+      service_url: 'http://example.com/icaas',
+      storage_url: 'https://pithos.example.com/object-store/v1',
+      compute_url: 'https://cyclades.example.com/',
+      storage_view_url: 'https://pithos.example.com/ui/view',
+      api_access_url: 'https://accounts.example.com/api_access',
+      copyright_url: 'https://www.grnet.gr/en',
+      terms_url: 'https://accounts.example.com/approval_terms'
+    }
   }
 
   return ENV;
